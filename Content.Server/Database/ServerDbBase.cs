fec1d578cce93a43bfdd80370b3bb72e44f358a6
rebaserHEAD <38984539+rebaserHEAD@users.noreply.github.com>
2026-06-01 20:28:59 -0400

    fix(db): restore UtcNow for consent timestamps
    The Real Traits merge incidentally flipped four consent timestamp writes in
    ServerDbBase (ConsentFreetextUpdatedAt x2, ReadAt x2) from DateTime.UtcNow
    to DateTime.Now, storing server-local time while the rest of the file and
    the schema use UTC. Restore UtcNow so timestamps stay comparable on
    non-UTC hosts.

--- Content.Server/Database/ServerDbBase.cs
+++ Content.Server/Database/ServerDbBase.cs
@@ -1982,7 +1982,7 @@ INSERT INTO player_round (players_id, rounds_id) VALUES ({players[player]}, {id}
                     UserId = userId,
                     ConsentToggles = new(),
                     ConsentFreetext = consentSettings.Freetext,
-                    ConsentFreetextUpdatedAt = DateTime.Now,
+                    ConsentFreetextUpdatedAt = DateTime.UtcNow,
                 };
 
                 db.DbContext.ConsentSettings.Add(currentConsentSettings);
@@ -1990,7 +1990,7 @@ INSERT INTO player_round (players_id, rounds_id) VALUES ({players[player]}, {id}
             else if (currentConsentSettings.ConsentFreetext != consentSettings.Freetext)
             {
                 currentConsentSettings.ConsentFreetext = consentSettings.Freetext;
-                currentConsentSettings.ConsentFreetextUpdatedAt = DateTime.Now;
+                currentConsentSettings.ConsentFreetextUpdatedAt = DateTime.UtcNow;
             }
 
             Dictionary<ProtoId<ConsentTogglePrototype>, string> currentConsentToggles = currentConsentSettings.ConsentToggles.ToDictionary(
@@ -2062,11 +2062,11 @@ INSERT INTO player_round (players_id, rounds_id) VALUES ({players[player]}, {id}
                 {
                     ReaderUserId = readerUserId,
                     ReadConsentSettingsId = readConsentSettingsId,
-                    ReadAt = DateTime.Now,
+                    ReadAt = DateTime.UtcNow,
                 };
             }
             else {
-                readRecipe.ReadAt = DateTime.Now;
+                readRecipe.ReadAt = DateTime.UtcNow;
             }
 
             return readRecipe;
